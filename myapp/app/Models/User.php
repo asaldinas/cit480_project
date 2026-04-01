6ba117493e8ca0ebc4538849cd04e01ba84f7edc
Abraham Saldinas <asaldinas2003@gmail.com>
2026-04-01 11:42:06 -0700

    clean branch for CI/CD

--- myapp/app/Models/User.php
+++ myapp/app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\VerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
+   
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -48,5 +50,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+    } 
+    public function sendEmailVerificationNotification()
+{
+    $this->notify(new VerifyEmail);
+}
 }
